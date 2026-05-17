94ed04dfe34636f671bfc109e7efdb4b7004596c
avesshdubey <aveshdubey648@gmail.com>
2026-05-17 12:29:59 +0530

    content

---
+++ 00.3_DOM_text&content/script.js
@@ -0,0 +1,10 @@
+console.log("somthing");
+const box = document.getElementById("box");
+console.log(box.innerText); //calculates, css,hidden elements and render text . Hidden not visible
+console.log(box.textContent);//ignore css,hidden elements and render text . Hidden visible, line break, space, all.
+
+console.log(box.innerHTML);//shows tags also
+
+// Excercise 1
+
+
